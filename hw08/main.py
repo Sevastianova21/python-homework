7bec49cdce1a9417cbd6128fd2c44ebd5767061e
Sevastianova Irina <Sevastianova21@gmail.com>
2023-03-13 21:25:27 +0300

    homework update

--- hw08/main.py
+++ hw08/main.py
@@ -8,21 +8,21 @@ print('Меню работы с телефонным справочником\n'
 '7. Удалить контакт\n'
 '8. Выход')
 what_to_do = 0
+contacts = []
 def open_contact():
     with open('phone.txt', 'r') as file:
         contacts = file.readlines()
         print(contacts)
-def save_contact():
+        return contacts
+def save_contact(contacts):
     data = open('phone.txt', 'w')
     for i in range (len(contacts)):
         data.write(contacts[i])
     data.close()
-def add_contact():
-    with open('phone.txt', 'r') as file:
-        contacts = file.readlines()
-    contact = input('Введите контакт')
-    contacts.append(contact)
-    print('Контакт добавлен в справочник')
+def add_contact(contacts):
+    contacts.append(input('Введите контакт '))
+    print('Контакт добавлен в справочник ')
+    return contacts
 def show_contact():
     data = open('phone.txt', 'r')
     i = 1
@@ -30,33 +30,30 @@ def show_contact():
         print(i, line)
         i = i + 1
     data.close()
-def rewrite_contact():
-    with open('phone.txt', 'r') as file:
-        contacts = file.readlines()
-    string_number = int(input('Введите номер строки, которую хотите изменить'))
-    new_contact = input('Введите новую информацию')
-    contacts[string_number] = new_contact
-def find_contact():
-    with open('phone.txt', 'r') as file:
-        contacts = file.readlines()
+def rewrite_contact(contacts):
+    string_number = int(input('Введите номер строки, которую хотите изменить '))
+    contacts[string_number-1] = input('Введите новую информацию ') + '\n'
+    return contacts
+def find_contact(contacts):
     find = input('Введите фамилию, имя или номер')
     for i in range(len(contacts)):
         contact = contacts.split()
         if find in contact:
             print(i, contacts[i])
-def delete_contact():
-    with open('phone.txt', 'r') as file:
-        contacts = file.readlines()
+
+def delete_contact(contacts):
     number = int(input('Введите номер строки контакта для удаления'))
     contacts = contacts.pop(number)
+    return contacts
+
 while what_to_do < 8:
     what_to_do = int(input('Введите номер соответствующего действия\n'))
     match what_to_do:
-        case 1: open_contact()
-        case 2: save_contact()
+        case 1: contacts = open_contact()
+        case 2: save_contact(contacts)
         case 3: show_contact()
-        case 4: add_contact()
-        case 5: rewrite_contact()
-        case 6: find_contact()
-        case 7: delete_contact()
+        case 4: contacts = add_contact(contacts)
+        case 5: contacts = rewrite_contact(contacts)
+        case 6: find_contact(contacts)
+        case 7: contacts = delete_contact(contacts)
         case 8: print('завершение работы со справочником')
